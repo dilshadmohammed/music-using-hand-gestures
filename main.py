82931287b1e4605f6150d74e0567107f408ee43a
Dilshad Mohammed <dilshadmohammed13@gmail.com>
2023-03-26 18:55:16 +0530

    second intrument added screen divided into left and right parts sitar in left and piano in right

--- main.py
+++ main.py
@@ -11,13 +11,20 @@ pygame.mixer.set_num_channels(25)
 sound_files = ['soundfiles/sitar1.wav', 'soundfiles/sitar2.wav', 'soundfiles/sitar3.wav',
                'soundfiles/sitar4.wav', 'soundfiles/sitar5.wav', 'soundfiles/sitar0.wav']
 
-# create an empty list for the sound objects
+sound_files1 = ['soundfiles/piano-Gnew.wav', 'soundfiles/piano-A.wav', 'soundfiles/piano-B.wav',
+                'soundfiles/piano-C.wav', 'soundfiles/piano-D.wav', 'soundfiles/piano-Fnew.wav']
+
+# create  empty lists for the sound objects
 sounds = []
+sounds1 = []
 
 # loop over the sound files and create Pygame sound objects for each file
-for file in sound_files:
+for file in sound_files:  # sitar
     sound = pygame.mixer.Sound(file)
     sounds.append(sound)
+for file in sound_files1:  # piano
+    sound = pygame.mixer.Sound(file)
+    sounds1.append(sound)
 
 # width and height of camera
 wCam, hCam = 1280, 720
@@ -66,19 +73,28 @@ while True:
         totalFingers = fingers.count(1)
         print(totalFingers)
 
-        sounds[totalFingers - 1].play()
-        time.sleep(0.15)
+        # check if hand is in right or left side of the screen
+        # If hand is in left sitar else piano
+        if lmList[0][1] < 640:
+            sounds[totalFingers - 1].play()
+            time.sleep(0.15)
 
-        cv2.rectangle(img, (10, hCam - 117), (85, hCam - 17), (13, 221, 224), cv2.FILLED)
-        cv2.putText(img, str(totalFingers), (23, hCam - 42), cv2.FONT_HERSHEY_PLAIN, 5, (235, 189, 23), 12)
+            cv2.rectangle(img, (10, hCam - 117), (85, hCam - 17), (13, 221, 224), cv2.FILLED)
+            cv2.putText(img, str(totalFingers), (23, hCam - 42), cv2.FONT_HERSHEY_PLAIN, 5, (235, 189, 23), 12)
+
+        else:
+            sounds1[totalFingers - 1].play()
+            time.sleep(0.1)
+
+            cv2.rectangle(img, (1185, hCam - 117), (1270, hCam - 17), (13, 221, 224), cv2.FILLED)
+            cv2.putText(img, str(totalFingers), (1198, hCam - 42), cv2.FONT_HERSHEY_PLAIN, 5, (235, 189, 23), 12)
 
     # calculating fps
     currentTime = time.time()
     fps = 1 / (currentTime - previousTime)
     previousTime = currentTime
-
+    
     cv2.putText(img, f'FPS: {int(fps)}', (wCam - 77, 23), cv2.FONT_HERSHEY_PLAIN, 1, (156, 2, 245), 1)
-
     cv2.imshow("Image", img)
 
     # Wait for a key press and check if the 'q' key was pressed
