1f94644afdaecf92034f59e928c48eb51c9d9160
Noel Tom Santhosh <inbox.noel1107@gmail.com>
2023-03-31 00:09:54 +0530

    frontend created

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ sound_files1 = ['soundfiles/piano-Gnew.wav', 'soundfiles/piano-A.wav', 'soundfil
 sounds = []
 sounds1 = []
 
+detector = htm.handDetector(detectionCon=0.75)
+
+left_hand_tipIds = [4, 8, 12, 16, 20]
+right_hand_tipIds = [4, 8, 12, 16, 20]
+
 # loop over the sound files and create Pygame sound objects for each file
 for file in sound_files:  # sitar
     sound = pygame.mixer.Sound(file)
@@ -26,146 +31,159 @@ for file in sound_files1:  # piano
     sound = pygame.mixer.Sound(file)
     sounds1.append(sound)
 
-# width and height of camera
+# Import necessary libraries
+from flask import Flask, render_template, Response, url_for
+
+# Initialize the Flask app
+app = Flask(__name__)
+
+from flask import Flask, render_template
+import cv2
+
 wCam, hCam = 1280, 720
-
-cap = cv2.VideoCapture(0)
-cap.set(3, wCam)
-cap.set(4, hCam)
-
-previousTime = 0
-
+camera = cv2.VideoCapture(0)
+camera.set(3, wCam)
+camera.set(4, hCam)
 detector = htm.handDetector(detectionCon=0.75)
 
 tipIds = [4, 8, 12, 16, 20]
 
-while True:
-    success, img = cap.read()
-    img = cv2.flip(img, 1)
 
-    img = detector.findHands(img)
-    lmList_left = detector.findPosition(img, draw=False, handNo=0)
-    lmList_right = detector.findPosition(img, draw=False, handNo=-1)
-    if lmList_right == lmList_left:
-        lmListCommon = lmList_right
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        frame = cv2.flip(frame, 1)
 
-    if len(lmList_left) != 0 and lmList_left != lmList_right:
-        fingers = []
-        # Checking left or right hand
-        if lmList_left[1][1] < lmList_left[0][1]:
-            # Thumb
-            if lmList_left[tipIds[0]][1] < lmList_left[tipIds[0] - 1][1]:
-                fingers.append(1)
+        frame = detector.findHands(frame)
+        lmList_left = detector.findPosition(frame, draw=False, handNo=0)
+        lmList_right = detector.findPosition(frame, draw=False, handNo=-1)
+        if lmList_right == lmList_left:
+            lmListCommon = lmList_right
+
+        if len(lmList_left) != 0 and lmList_left != lmList_right:
+            fingers = []
+            # Checking left or right hand
+            if lmList_left[1][1] < lmList_left[0][1]:
+                # Thumb
+                if lmList_left[tipIds[0]][1] < lmList_left[tipIds[0] - 1][1]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
             else:
-                fingers.append(0)
-        else:
-            # Thumb
-            if lmList_left[tipIds[0]][1] > lmList_left[tipIds[0] - 1][1]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
+                # Thumb
+                if lmList_left[tipIds[0]][1] > lmList_left[tipIds[0] - 1][1]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
 
-        # 4 Fingers
-        for id in range(1, 5):
-            if lmList_left[tipIds[id]][2] < lmList_left[tipIds[id] - 2][2]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
+            # 4 Fingers
+            for id in range(1, 5):
+                if lmList_left[tipIds[id]][2] < lmList_left[tipIds[id] - 2][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
 
-        # print(fingers)
-        totalFingers = fingers.count(1)
+            # print(fingers)
+            totalFingers = fingers.count(1)
 
-        sounds[totalFingers - 1].play()
-        time.sleep(0.1)
-
-        cv2.rectangle(img, (10, hCam - 117), (85, hCam - 17), (13, 221, 224), cv2.FILLED)
-        cv2.putText(img, str(totalFingers), (23, hCam - 42), cv2.FONT_HERSHEY_PLAIN, 5, (235, 189, 23), 12)
-
-    if len(lmList_right) != 0 and lmList_left != lmList_right:
-        fingers = []
-        # Checking left or right hand
-        if lmList_right[1][1] < lmList_right[0][1]:
-            # Thumb
-            if lmList_right[tipIds[0]][1] < lmList_right[tipIds[0] - 1][1]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
-        else:
-            # Thumb
-            if lmList_right[tipIds[0]][1] > lmList_right[tipIds[0] - 1][1]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
-
-        # 4 Fingers
-        for id in range(1, 5):
-            if lmList_right[tipIds[id]][2] < lmList_right[tipIds[id] - 2][2]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
-
-        # print(fingers)
-        totalFingers = fingers.count(1)
-        sounds1[totalFingers - 1].play()
-        time.sleep(0.1)
-
-        cv2.rectangle(img, (1185, hCam - 117), (1270, hCam - 17), (13, 221, 224), cv2.FILLED)
-        cv2.putText(img, str(totalFingers), (1198, hCam - 42), cv2.FONT_HERSHEY_PLAIN, 5, (235, 189, 23), 12)
-    if len(lmListCommon) != 0:
-        fingers = []
-        # Checking left or right hand
-        if lmListCommon[1][1] < lmListCommon[0][1]:
-            # Thumb
-            if lmListCommon[tipIds[0]][1] < lmListCommon[tipIds[0] - 1][1]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
-        else:
-            # Thumb
-            if lmListCommon[tipIds[0]][1] > lmListCommon[tipIds[0] - 1][1]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
-
-        # 4 Fingers
-        for id in range(1, 5):
-            if lmListCommon[tipIds[id]][2] < lmListCommon[tipIds[id] - 2][2]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
-
-        # print(fingers)
-        totalFingers = fingers.count(1)
-
-        # check if hand is in right or left side of the screen
-        # If hand is in left sitar else piano
-        if lmListCommon[0][1] < 640:
             sounds[totalFingers - 1].play()
-            time.sleep(0.15)
+            time.sleep(0.01)
+            #cv2.circle(frame, (1228, hCam - 60), 38, (188, 241, 288), cv2.FILLED)
+            #cv2.putText(frame, str(totalFingers), (1203, hCam - 33), cv2.FONT_HERSHEY_PLAIN, 5, (62, 78, 240), 12)
 
-            cv2.rectangle(img, (10, hCam - 117), (85, hCam - 17), (13, 221, 224), cv2.FILLED)
-            cv2.putText(img, str(totalFingers), (23, hCam - 42), cv2.FONT_HERSHEY_PLAIN, 5, (235, 189, 23), 12)
 
-        else:
+
+        if len(lmList_right) != 0 and lmList_left != lmList_right:
+            fingers = []
+            # Checking left or right hand
+            if lmList_right[1][1] < lmList_right[0][1]:
+                # Thumb
+                if lmList_right[tipIds[0]][1] < lmList_right[tipIds[0] - 1][1]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+            else:
+                # Thumb
+                if lmList_right[tipIds[0]][1] > lmList_right[tipIds[0] - 1][1]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+
+            # 4 Fingers
+            for id in range(1, 5):
+                if lmList_right[tipIds[id]][2] < lmList_right[tipIds[id] - 2][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+
+            # print(fingers)
+            totalFingers = fingers.count(1)
             sounds1[totalFingers - 1].play()
-            time.sleep(0.1)
+            time.sleep(0.01)
+            #cv2.circle(frame, (55, hCam - 60), 38, (188, 241, 288), cv2.FILLED)
+            #cv2.putText(frame, str(totalFingers), (30, hCam - 33), cv2.FONT_HERSHEY_PLAIN, 5, (62, 78, 240), 12)
 
-            cv2.rectangle(img, (1185, hCam - 117), (1270, hCam - 17), (13, 221, 224), cv2.FILLED)
-            cv2.putText(img, str(totalFingers), (1198, hCam - 42), cv2.FONT_HERSHEY_PLAIN, 5, (235, 189, 23), 12)
+        if len(lmListCommon) != 0:
+            fingers = []
+            # Checking left or right hand
+            if lmListCommon[1][1] < lmListCommon[0][1]:
+                # Thumb
+                if lmListCommon[tipIds[0]][1] < lmListCommon[tipIds[0] - 1][1]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+            else:
+                # Thumb
+                if lmListCommon[tipIds[0]][1] > lmListCommon[tipIds[0] - 1][1]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
 
-    # calculating fps
-    currentTime = time.time()
-    fps = 1 / (currentTime - previousTime)
-    previousTime = currentTime
+            # 4 Fingers
+            for id in range(1, 5):
+                if lmListCommon[tipIds[id]][2] < lmListCommon[tipIds[id] - 2][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
 
-    cv2.putText(img, f'FPS: {int(fps)}', (wCam - 77, 23), cv2.FONT_HERSHEY_PLAIN, 1, (156, 2, 245), 1)
-    cv2.imshow("Image", img)
+            # print(fingers)
+            totalFingers = fingers.count(1)
 
-    # Wait for a key press and check if the 'q' key was pressed
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        break
+            # check if hand is in right or left side of the screen
+            # If hand is in left sitar else piano
+            if lmListCommon[0][1] < 640:
+                sounds[totalFingers - 1].play()
+                # cv2.circle(frame, (55, hCam - 60), 38, (188, 241, 288), cv2.FILLED)
+                # cv2.putText(frame, str(totalFingers), (30, hCam - 33), cv2.FONT_HERSHEY_PLAIN, 5, (62, 78, 240), 12)
+                time.sleep(0.01)
 
-# Release the resources
-cap.release()
-cv2.destroyAllWindows()
+
+
+            else:
+                sounds1[totalFingers - 1].play()
+                # cv2.circle(frame, (1228, hCam - 60), 38, (188, 241, 288), cv2.FILLED)
+                # cv2.putText(frame, str(totalFingers), (1203, hCam - 33), cv2.FONT_HERSHEY_PLAIN, 5, (62, 78, 240), 12)
+                time.sleep(0.01)
+
+
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
